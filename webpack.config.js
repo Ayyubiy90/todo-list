dc44a573c96192d08931884c24eab8f3e880c9bf
AYB <ayyubiy67@gmail.com>
2024-07-21 18:19:53 +0100

    Updated the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development", // Set mode to 'development'
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'development',
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
   },
 };
